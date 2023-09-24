51798d931b7ecdf539b3afe31a0470433d503141
Anirudh Krishna kumar <anirudhkrishnakumar@wireless-nat-inside.uiowa.edu>
2023-09-23 20:04:43 -0500

    add new changes

---
+++ js/questions.js
@@ -0,0 +1,3 @@
+document.getElementById('submitAnswers').addEventListener('click', function() {
+    window.location.href = 'feedback.html';
+});
---
+++ js/script.js
@@ -0,0 +1,4 @@
+document.getElementById('submitBtn').addEventListener('click', function() {
+    console.log('Button clicked.');
+    window.location.href = 'questions.html';
+});
